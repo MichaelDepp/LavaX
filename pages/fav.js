2099b6924e4de39fb98b05f6cc33f6de764f9f03
MichaelDepp <michaeldepp7@gmail.com>
2020-09-24 16:24:02 +0800

    job page

--- pages/fav.js
+++ pages/fav.js
@@ -5,7 +5,7 @@ import {observer} from "mobx-react"
 
 const Fav = (props) => {
     return (
-        <div className="init bg-dark-200 min-h-screen">
+        <div className="bg-dark-200 min-h-screen">
             <div className="container p-6 bg-dark-200">
                 <Navbar></Navbar>
                 {/* <p className="text-primary font-black text-4xl text-center">Displays favourited jobs</p> */}
